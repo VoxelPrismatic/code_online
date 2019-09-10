74af305244c1e72dee8aae1150aecbc193f70220
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-10 16:54:48 -0500

    Update execjs.js

--- execjs.js
+++ execjs.js
@@ -1,23 +1,22 @@
 (function(){
-                    oldLog = console.log;
-                    console.log = function(...args){ 
-                        for (arg of args) { stdout += arg+""; }
-                        stdout += "\n";
-                    };})();
-                try { 
-                    stdrtn = eval("(function(){"+stdinp+";})();"); 
-                } catch(ex) { 
-                    stderr = ex.message; 
-                }
-                var txtinp = document.createTextNode(stdinp || "'?.code=' tag not found;");
-                var txtout = document.createTextNode(stdout.replace('\n', '<br >') || "*~");
-                var txtrtn = document.createTextNode((typeof stdrtn)+" ] "+stdrtn);
-                var txterr = document.createTextNode(stderr || "*~");
-                sysinp.appendChild(txtinp);
-                sysout.appendChild(txtout);
-                sysrtn.appendChild(txtrtn);
-                syserr.appendChild(txterr);
-                document.body.appendChild(sysinp);
-                document.body.appendChild(sysout);
-                document.body.appendChild(syserr);
-                document.body.appendChild(sysrtn);
+    oldLog = console.log;
+    console.log = function(...args){ 
+        for (arg of args) { stdout += arg+""; }
+        stdout += "\n";};})();
+try { 
+    stdrtn = eval("(function(){"+stdinp+";})();");
+} catch(ex) { 
+    stderr = ex.message; 
+}
+var txtinp = document.createTextNode(stdinp || "'?.code=' tag not found;");
+var txtout = document.createTextNode(stdout.replace('\n', '<br >') || "*~");
+var txtrtn = document.createTextNode((typeof stdrtn)+" ] "+stdrtn);
+var txterr = document.createTextNode(stderr || "*~");
+sysinp.appendChild(txtinp);
+sysout.appendChild(txtout);
+sysrtn.appendChild(txtrtn);
+syserr.appendChild(txterr);
+document.body.appendChild(sysinp);
+document.body.appendChild(sysout);
+document.body.appendChild(syserr);
+document.body.appendChild(sysrtn);
