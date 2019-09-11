546fd1839f654c0165950d2eed8268f8fcb14f0e
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-11 08:13:53 -0500

    Update execpy.py

--- execpy.py
+++ execpy.py
@@ -1,31 +1,11 @@
-#from urllib.parse import unquote
 from traceback import format_exc as fex
 from browser import document as doc, alert
-url = str(doc.URL)
-code = ""
-tokens = url.split("?.")
-for token in tokens:
-    if token.startswith('code='):
-        code = token[5:]
-skipA = False
-skipB = False
-stdinp = ""
-for x in range(len(code)):
-    if skipA: continue
-    elif skipB: continue
-    elif '%' in code[x:x+3]:
-        skipA, skipB = True, True
-        stdinp += eval(f'"\\u00{code[x+1:x+2]}"')
-    else:
-        stdinp += code[x]
-if not stdinp: stdinp = "pass"
+stdinp = doc.getElementById("CODE").innerHTML
 stdinp = '\n'.join('   '+line for line in stdinp.splitlines())
 stdrtn, stderr, stdout = "", "", ""
-sysinp = doc.createElement("DIV")
 sysout = doc.createElement("DIV")
 syserr = doc.createElement("DIV")
 sysrtn = doc.createElement("DIV")
-sysinp.style.color = "#ff00ffff"
 sysout.style.color = "#00ffffff"
 sysrtn.style.color = "#00ff00ff"
 syserr.style.color = "#ff0000ff"
@@ -40,16 +20,13 @@ exec('def fn():\n'+stdinp, tmp)
 try: 
     stdrtn = tmp['fn']()
 except Exception as exc: 
-    stderr = str(exc)+'<br />'+str(fex()).replace('\n', '<br />\u200b').replace(' ','\u200b \u200b')
-txtinp = doc.createTextNode(stdinp or "'?.code=' tag not found;")
+    stderr = str(exc)+'<br />'+str(fex()).replace('\n', '\n\u200b').replace(' ','\u200b \u200b')
 txtout = doc.createTextNode(stdout or "*~")
 txtrtn = doc.createTextNode((str(type(stdrtn)).split("'")[1])+" ] "+str(stdrtn))
 txterr = doc.createTextNode(stderr or "*~")
-sysinp.append(txtinp)
 sysout.append(txtout)
 sysrtn.append(txtrtn)
 syserr.append(txterr)
-doc.body.append(sysinp)
 doc.body.append(sysout)
 doc.body.append(syserr)
 doc.body.append(sysrtn)
