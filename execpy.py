8559107abcf787336cd145bd7a108e8726ae1401
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-11 11:40:20 -0500

    Update execpy.py

--- execpy.py
+++ execpy.py
@@ -1,13 +1,7 @@
 from browser import document as doc, alert
-stdinp = doc.getElementById("CODE").innerHTML
+stdinp = doc.getElementById("SYSINP").innerHTML or "return None"
 stdinp = '\n'.join('   '+line for line in stdinp.splitlines())
 stdrtn, stderr, stdout = "", "", ""
-sysout = doc.createElement("DIV")
-syserr = doc.createElement("DIV")
-sysrtn = doc.createElement("DIV")
-sysout.style.color = "#00ffffff"
-sysrtn.style.color = "#00ff00ff"
-syserr.style.color = "#ff0000ff"
 old_print = print
 def print(*args, sep=' ', end='\n'):
     global stdout
@@ -21,9 +15,10 @@ try:
     stdrtn = tmp['fn']()
 except Exception as ex: 
     stderr = str(ex)
-txtrtn = doc.createTextNode((str(type(stdrtn)).split("'")[1])+" ] "+str(stdrtn))
-txterr = doc.createTextNode(stderr or "~")
-sysrtn.append(txtrtn)
-syserr.append(txterr)
-doc.body.append(syserr)
-doc.body.append(sysrtn)
+sysout = (sysout or "~")
+syserr = (syserr or "~")
+sysRTN = (sysout or "~")
+doc.getElementById("SYSINP") = sysinp.replace('\n','<br>').replace(' ','\u200b')
+doc.getElementById("SYSOUT") = sysout.replace('\n','<br>').replace(' ','\u200b')
+doc.getElementById("SYSERR") = syserr.replace('\n','<br>').replace(' ','\u200b')
+doc.getElementById("SYSRTN") = sysinp.replace('\n','<br>').replace(' ','\u200b')
