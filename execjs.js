262821a5879afb16670562d0e14f9f1ff04dfbce
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-11 08:13:33 -0500

    Update execjs.js

--- execjs.js
+++ execjs.js
@@ -1,23 +1,13 @@
-var url = document.URL+"";
-var tokens = url.split("?.");
-var code = "";
-for (token of tokens) {
-    if (token.search("code=")==0) {
-        code += token.slice(5)+"";
-    }
-}
-var sysinp = document.createElement("DIV");
 var sysout = document.createElement("DIV");
 var sysrtn = document.createElement("DIV");
 var syserr = document.createElement("DIV");
-sysinp.style.color = "#ff00ffff";
 sysout.style.color = "#00ffffff";
 sysrtn.style.color = "#00ff00ff";
 syserr.style.color = "#ff0000ff";
 var stderr = "";
 var stdout = "";
-var stdinp = decodeURIComponent(code)+"";
 var stdrtn;
+var stdinp = document.getElementById("CODE").inmerHTML;
 (function(){
     oldLog = console.log;
     console.log = function(...args){ 
@@ -28,15 +18,12 @@ try {
 } catch(ex) { 
     stderr = ex.message; 
 }
-var txtinp = document.createTextNode(stdinp || "'?.code=' tag not found;");
 var txtout = document.createTextNode(stdout.replace('\n', '<br>.') || "*~");
 var txtrtn = document.createTextNode((typeof stdrtn)+" ] "+stdrtn);
 var txterr = document.createTextNode(stderr || "*~");
-sysinp.appendChild(txtinp);
 sysout.appendChild(txtout);
 sysrtn.appendChild(txtrtn);
 syserr.appendChild(txterr);
-document.body.appendChild(sysinp);
 document.body.appendChild(sysout);
 document.body.appendChild(syserr);
 document.body.appendChild(sysrtn);
