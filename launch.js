529951872f74e6132513ed6591e54d3d66886dcd
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-11 13:22:42 -0500

    Create launch.js

---
+++ launch.js
@@ -0,0 +1,31 @@
+/* LOADER */
+            var executor = document.createElement("SCRIPT");
+            // NO CODE
+            if (stdinp == "") {
+                document.getElementById("SYSLNG").innerHTML = "LANG ] *.*";
+                document.getElementById("SYSOUT").innerHTML = "~";
+                document.getElementById("SYSERR").innerHTML = "'?.code=' tag not provided in URL";
+                document.getElementById("SYSRTN").innerHTML = "? ] ?";
+            }
+            else if (jskw.indexOf(lang) > -1) {
+                executor.type = "text/javascript";
+                executor.src = "execjs.js";
+                document.getElementById("SYSLNG").innerHTML = "LANG ] *.JS";
+                document.body.appendChild(executor);
+            }
+            
+            // PY
+            else if (pykw.indexOf(lang) > -1) {
+                executor.type = "text/python";
+                executor.src = "execpy.py";
+                document.getElementById("SYSLNG").innerHTML = "LANG ] *.PY";
+                document.body.appendChild(executor);
+            }
+            
+            // Not supported
+            else {
+                document.getElementById("SYSLNG").innerHTML = "LANG ] *.*";
+                document.getElementById("SYSOUT").innerHTML = "Supported languages: 'js', 'py'";
+                document.getElementById("SYSERR").innerHTML = "'?.lang=' tag not provided in URL";
+                document.getElementById("SYSRTN").innerHTML = "? ] ?";
+            }
