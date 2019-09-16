5d5bd469ffd06ef86596df2aad8659ce5c1419ac
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-16 16:18:27 -0500

    Update execjs.js

--- exe/execjs.js
+++ exe/execjs.js
@@ -12,7 +12,7 @@ try {
 } catch(ex) { 
     stderr = ex.message; 
 }
-document.getElementById("SYSINP").innerHTML = "(function(){\n"+(stdinp.replace('\n','\n    '))+"\n;})();";
+document.getElementById("SYSINP").innerHTML = "(function(){\n"+stdinp+"\n;})();";
 document.getElementById("SYSOUT").innerHTML = stdout;
 document.getElementById("SYSERR").innerHTML = stderr;
 document.getElementById("SYSRTN").innerHTML = (typeof stdrtn)+" ] "+stdrtn;
