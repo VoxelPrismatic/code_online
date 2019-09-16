aab8a91efa417372e3aaab429d74a55a4f02ceb3
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-16 11:05:56 -0500

    Update exectruth.py

--- exe/exectruth.py
+++ exe/exectruth.py
@@ -1,1 +1,21 @@
-
+from browser import document as doc, alert
+stdinp = (doc.getElementById("SYSINP").innerHTML or "A")
+args = []
+gate = []
+for x in stdinp:
+    if x == ' ':
+        continue
+    if x == '!':
+        args.append('not')
+    elif x == '+':
+        args.append('or')
+    elif x == '?':
+        args.append('!=')
+    else:
+        args.append(x)
+        gate.append(False)
+eq = ' '.join(args)
+doc.getElementById("SYSINP").innerHTML = stdinp
+doc.getElementById("SYSOUT").innerHTML = eq
+doc.getElementById("SYSERR").innerHTML = "SYNTAX: A!BC+D?E - (A AND NOT B AND C) OR (D XOR E)"
+doc.getElementById("SYSRTN").innerHTML = f''
