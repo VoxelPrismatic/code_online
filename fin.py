44a4cf5f2fc2e516b4f110f907b89855a42c40c0
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-16 18:14:26 -0500

    Update fin.py

--- fin.py
+++ fin.py
@@ -1,7 +1,11 @@
 from browser import document
 for x in ['SYSLNG','SYSINP','SYSOUT','SYSERR','SYSRTN']:
     itm = document.getElementById(x)
-    itm.innerHTML = itm.innerHTML.replace('\n','<br>').replace(' ','\u200b \u200b')
+    rep = {'\n':'<br>',
+           '\t': '    ',
+           ' ': '\x0b \x0b'}
+    for r in rep:
+        itm.innerHTML = itm.innerHTML.replace(r, rep[r])
 document.body.style.backgroundColor = "#112222ff"
 document.body.style.color = "#ffffffff"
 document.body.style.fontFamily = "Ubuntu Mono"
