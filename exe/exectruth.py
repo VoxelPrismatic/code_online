2c7cbce5112ff5a41a17e719129edbf74e785192
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-16 13:20:16 -0500

    Update exectruth.py

--- exe/exectruth.py
+++ exe/exectruth.py
@@ -21,7 +21,7 @@ for x in range(len(stdinp)-1):
         gate.append('0')
         lttr.append(stdinp[x])
 eq = ' '.join(args)
-doc.getElementById("SYSOUT").innerHTML = eq
+doc.getElementById("SYSRTN").innerHTML = eq
 def next(gate):
     return list(f'{int("".join(gate),2)+1:b}'.zfill(len(gate)))
 def calc(gate):
@@ -37,7 +37,6 @@ while any(g == '0' for g in gate):
     stdout += '<br>'+calc(gate)
     gate = next(gate)
 eq1 = eq
-stdout += '<br>'+calc(gate)          
-
+stdout += '<br>'+calc(gate)
+doc.getElementById("SYSOUT").innerHTML = stdout
 doc.getElementById("SYSERR").innerHTML = "SYNTAX: A!BC+D:E - (A AND NOT B AND C) OR (D XOR E)"
-doc.getElementById("SYSRTN").innerHTML = stdout
