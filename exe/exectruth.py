64c29ce1b477c49866495b571b7331c5cdfe0c3e
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-16 11:28:46 -0500

    Update exectruth.py

--- exe/exectruth.py
+++ exe/exectruth.py
@@ -15,8 +15,10 @@ for x in range(len(stdinp)-1):
         args.append(stdinp[x])
         if stdinp[x+1].lower() in 'abcdefghijklmnopqrstuvwxyz':
             args.append('and')
-        gate.append(0)
+        gate.append('0')
 eq = ' '.join(args)
+def next(gate):
+    return list(f'{int("".join(gate),2)+1:b}')
 doc.getElementById("SYSINP").innerHTML = stdinp
 doc.getElementById("SYSOUT").innerHTML = eq
 doc.getElementById("SYSERR").innerHTML = "SYNTAX: A!BC+D?E - (A AND NOT B AND C) OR (D XOR E)"
