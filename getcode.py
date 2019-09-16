4bc3877244811f536161508e621e584b7e20b0bc
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-16 18:08:27 -0500

    Update getcode.py

--- getcode.py
+++ getcode.py
@@ -1,2 +1,5 @@
 from browser import document
 document.getElementById("SYSINP").innerHTML = eval(f'"{document.URL.split("?.")[1][5:].replace("%","\\x")}"')
+script = document.createElement("SCRIPT")
+script.src = "launch.js"
+document.body.appendChild(script)
