94005263f9d0c2ab4759c21aba1cbc38e5af496a
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-16 17:29:40 -0500

    Update getcode.py

--- getcode.py
+++ getcode.py
@@ -1,2 +1,2 @@
 from browser import document, window
-document.getElementById("SYSINP").innerHTML = eval(f'"{document.URL.split("?.")[1].replace("%","\\x")}"')
+document.getElementById("SYSINP").innerHTML = eval(f'"{document.URL.split("?.")[1][5:].replace("%","\\x")}"')
