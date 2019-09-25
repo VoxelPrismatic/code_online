0f166c8646c9859fc051af9956f76857a9f5d3ac
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-25 11:15:09 -0500

    Update exectruth.py

--- exe/exectruth.py
+++ exe/exectruth.py
@@ -69,7 +69,7 @@ def calc(gate):
         st += f"-{int(eval(eq1))}-"
         st += " | "
     return '| '+st
-stdout = '| '+' | '.join(' '.join(lttr) + ' OUT' for x in eq.split('|'))+' |'
+stdout = '| '+' | '.join(' '.join(lttr+['OUT']) for x in eq.split('|'))+' |'
 stdout += '\n'+'-'*len(stdout)
 while any(g == '0' for g in gate):
     stdout += '\n'+calc(gate)
