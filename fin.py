3e42a60e1728c03d13b1bc8e2e4a92621a1166a3
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-14 21:57:27 -0500

    Update fin.py

--- fin.py
+++ fin.py
@@ -3,9 +3,7 @@ for x in ['SYSLNG','SYSINP','SYSOUT','SYSERR','SYSRTN']:
     itm = document.getElementById(x)
     rep = {'\n':'<br>',
            '\t': '    ',
-           ' ': ' \u200b',
-           '>': '&gt;',
-           '<': '&lt;'}
+           ' ': ' \u200b'}
     for r in rep:
         itm.innerHTML = itm.innerHTML.replace(r, rep[r])
 document.body.style.backgroundColor = "#112222ff"
